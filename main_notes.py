4472b0b4cec139c8a03bd9fdabeb1a26cf8de918
Anastasia <anastasija2003@mail.ru>
2023-08-23 18:59:51 +0300

    Interim

---
+++ main_notes.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from menu import main_Menu
+from Note import *
+from dump import *
+import os
+
+
+
+
+# =================== Основной блок =================================
+
+os.system("CLS")
+print()
+print()
+print()
+print()
+
+work_array = readFromCSV("notes.csv")
+saveToCSV(work_array, "notes.bak")
+print()
+work_array = main_Menu(work_array)
+saveToCSV(work_array, "notes.csv")
